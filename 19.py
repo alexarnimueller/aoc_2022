f479d55995aafb6f948ceb5b894322105debfdde
Alex Müller <alexarnimueller@protonmail.com>
2022-12-20 19:16:49 +0100

    wip 19

--- 19.py
+++ 19.py
@@ -22,39 +22,50 @@ class Blueprint(object):
         self.ore = 0
         self.cly = 0
         self.obs = 0
-        self.cracked = 0
+        self.geo = 0
 
     def __repr__(self) -> str:
         return f"Blueprint {self.number}"
 
     def run(self) -> int:
         while self.minutes:
+            new_ore_robot = 0
+            new_cly_robot = 0
+            new_obs_robot = 0
+            new_geo_robot = 0
+
+            if self.ore >= self.geo_robot_ore_cost and self.obs >= self.geo_robot_ore_cost:
+                new_geo_robots = 1
+                self.ore -= self.geo_robot_ore_cost 
+                self.obs -= self.geo_robot_ore_cost                     
+
+            elif self.ore >= self.obs_robot_ore_cost and self.cly >= self.obs_robot_cly_cost:
+                new_obs_robots = 1
+                self.ore -= self.obs_robot_ore_cost 
+                self.cly -= self.obs_robot_cly_cost
+ 
+            elif self.ore >= self.cly_robot_ore_cost:
+                new_cly_robots = 1
+                self.ore -= self.cly_robot_ore_cost
+            
+            elif self.ore >= self.ore_robot_ore_cost:
+                new_ore_robot = 1
+                self.ore -= self.ore_robot_ore_cost
+            
+            
             self.ore += self.ore_robots
             self.cly += self.cly_robots
             self.obs += self.obs_robots
-            self.cracked += self.geo_robots               
-            
-            if self.ore >= self.ore_robot_ore_cost:
-                self.ore_robots += 1
-                self.ore -= self.ore_robot_ore_cost
-            
-            elif self.ore >= self.cly_robot_ore_cost:
-                self.cly_robots += 1
-                self.ore -= self.cly_robot_ore_cost
-            
-            elif self.ore >= self.obs_robot_ore_cost and self.cly >= self.obs_robot_cly_cost:
-                self.obs_robots += 1
-                self.ore -= self.obs_robot_ore_cost 
-                self.cly -= self.obs_robot_cly_cost
-            
-            elif self.ore >= self.geo_robot_ore_cost and self.obs >= self.geo_robot_ore_cost:
-                self.geo_robots += 1
-                self.ore -= self.geo_robot_ore_cost 
-                self.obs -= self.geo_robot_ore_cost                     
+            self.geo += self.geo_robots               
+         
+            self.ore_robots += new_ore_robot
+            self.cly_robots += new_cly_robot
+            self.obs_robots += new_obs_robot
+            self.geo_robots += new_geo_robot
             
             self.minutes -= 1
             
-        return self.cracked * self.number
+        return self.geo * self.number
 
 
 def read_input(filename):
@@ -65,6 +76,6 @@ def read_input(filename):
 
 
 blueprints = read_input('19.test')
-print(max(bp.run() for bp in blueprints))
+print(sum(bp.run() for bp in blueprints))
 for bp in blueprints:
     print(bp.__dict__)
